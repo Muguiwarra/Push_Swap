047cd598b949d60377df101b228ec5b1c8344fb6
Nouh Abboune <nabboune@e1r10p3.1337.ma>
2023-01-28 03:59:27 +0100

    Protection from NULL String

--- help_functions_0.c
+++ help_functions_0.c
@@ -6,7 +6,7 @@
 /*   By: nabboune <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/26 16:36:14 by nabboune          #+#    #+#             */
-/*   Updated: 2023/01/27 02:35:57 by nabboune         ###   ########.fr       */
+/*   Updated: 2023/01/28 03:58:13 by nabboune         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,20 @@ char	***ft_split_arguments(int ac, char **av)
 	char	***cttab;
 	int		i;
 	int		j;
+	int		k;
 
 	i = 1;
 	j = 0;
+	k = 0;
 	cttab = (char ***)malloc(ac * sizeof(char **));
 	if (!cttab)
 		return (0);
 	while (av[i])
-		cttab[j++] = ft_split(av[i++], ' ');
+	{
+		cttab[j] = ft_split(av[i++], ' ');
+		if (!cttab[j++][k])
+			return (0);
+	}
 	cttab[j] = 0;
 	return (cttab);
 }
